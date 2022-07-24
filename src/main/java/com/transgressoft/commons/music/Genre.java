27bd5f4adafd4ca7b366713a0bfa8d9100adfdd6
Octavio Calleya Garcia <octavio@transgressoft.net>
2022-07-24 23:39:39 +0200

    Change in Genre

--- src/main/java/com/transgressoft/commons/music/Genre.java
+++ src/main/java/com/transgressoft/commons/music/Genre.java
@@ -7,7 +7,19 @@ public enum Genre {
 
     UNDEFINED;
 
-    static Genre parseGenre(String value) {
+    public static Genre parseGenre(String value) {
         return UNDEFINED;
+    }
+
+    public String capitalize() {
+        String replaced = this.name().replace('_', ' ');
+
+        char[] capitalized = new char[replaced.length()];
+        capitalized[0] = Character.toTitleCase(replaced.charAt(0));
+        for (int c = 1; c < replaced.toCharArray().length; c++)
+            if (replaced.charAt(c - 1) == ' ' || replaced.charAt(c - 1) == ',')
+                capitalized[c] = Character.toTitleCase(replaced.charAt(c));
+
+        return new String(capitalized);
     }
 }
